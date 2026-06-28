3f86032e7751495343eea586394b57d01f7ed0e0
karan-web-01 <karankumar95702@gmail.com>
2026-06-28 23:16:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function uploadDone() {
+    alert("Notes uploaded successfully! (Demo)");
+}
+
+function downloadDone(noteName) {
+    alert("Downloading: " + noteName);
+}
+
+function msgDone() {
+    alert("Message sent successfully!");
+}
